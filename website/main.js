02a8d9d661729879d27c4f41eb9f8edf187f9e7a
Mateusz Fabisiak <mfabisiak@student.agh.edu.pl>
2025-12-06 18:14:57 +0100

    Add passkey request

--- website/main.js
+++ website/main.js
@@ -1,17 +1,32 @@
 const verifyBtn = document.getElementById("verify-btn");
-const registerBtn = document.getElementById("register-btn")
+// const registerBtn = document.getElementById("register-btn")
+
+function arrayBufferToBase64Url(buffer) {
+    const bytes = new Uint8Array(buffer);
+    let binary = '';
+    for (let i = 0; i < bytes.byteLength; i++) {
+        binary += String.fromCharCode(bytes[i]);
+    }
+    const base64 = btoa(binary);
+    return base64.replace(/\+/g, '-').replace(/\//g, '_').replace(/=/g, '');
+}
 
 async function verify(){
     console.log("Verifying...");
 
-    const _options = await fetch("http://localhost:3000/signinRequest");
+    const challenge = crypto.getRandomValues(new Uint8Array(32));
 
-    const decoded_options = await _options.json();
+    const decoded_options = {
+        allowCredentials: [],
+        challenge: arrayBufferToBase64Url(challenge.buffer),
+        rpId: "localhost",
+        timeout: 60000
+    }
 
     const options = PublicKeyCredential.parseRequestOptionsFromJSON(decoded_options);
     console.log(options)
 
-    const credential = await navigator.credentials.get({
+    await navigator.credentials.get({
         publicKey: options
 
     })
@@ -32,4 +47,4 @@ async function registerKey() {
 
 verifyBtn.addEventListener("click", verify);
 
-registerBtn.addEventListener("click", registerKey);
+// registerBtn.addEventListener("click", registerKey);
